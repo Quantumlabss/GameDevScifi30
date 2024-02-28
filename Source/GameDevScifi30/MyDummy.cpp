96d371bb3573b6c45f5d530ad3784d6fd034eebe
Canyon Anderson <Canyon.Anderson@fsd38.ab.ca>
2024-02-28 14:40:47 -0700

    vfgfgf

---
+++ Source/GameDevScifi30/MyDummy.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyDummy.h"
+
+// Sets default values
+AMyDummy::AMyDummy()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	  
+}
+
+// Called when the game starts or when spawned
+void AMyDummy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyDummy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	//while (boxCollider)
+}
+
+// Called to bind functionality to input
+void AMyDummy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
