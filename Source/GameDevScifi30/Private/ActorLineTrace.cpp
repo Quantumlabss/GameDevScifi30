912633b3f549711859e27eb262bff678d9669a75
Quantumlabss <alex.spadyevans@icloud.com>
2024-02-29 13:54:14 -0700

    raycast tests

--- Source/GameDevScifi30/Private/ActorLineTrace.cpp
+++ Source/GameDevScifi30/Private/ActorLineTrace.cpp
@@ -1,17 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "ActorLineTrace.h"
-
 //#include "ConstructorHelpers.h"
-
 #include "DrawDebugHelpers.h"
 
 // Sets default values
 AActorLineTrace::AActorLineTrace()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -27,13 +21,16 @@ AActorLineTrace::AActorLineTrace()
 		Cube->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
 		Cube->SetWorldScale3D(FVector(1.f));
 	}
+
+	// add another component in the editor to the actor to overlap with the line trace to get the event to fire
+
 }
 
 // Called when the game starts or when spawned
 void AActorLineTrace::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -48,7 +45,7 @@ void AActorLineTrace::Tick(float DeltaTime)
 	Start.X += 200.f;
 
 	FVector ForwardVector = GetActorForwardVector();
-	FVector  End = ((ForwardVector * 500.0f) + Start);
+	FVector End = ((ForwardVector * 500.f) + Start);
 	FCollisionQueryParams CollisionParams;
 
 	DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 1, 0, 5);
@@ -56,8 +53,9 @@ void AActorLineTrace::Tick(float DeltaTime)
 	if (ActorLineTraceSingle(OutHit, Start, End, ECC_WorldStatic, CollisionParams))
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Green, FString::Printf(TEXT("The Component Being Hit is: %s"), *OutHit.GetComponent()->GetName()));
+	
+		//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT(OutHit.GetComponent()->GetName()));
 	}
-
 
 }
 
