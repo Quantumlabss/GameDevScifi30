5c6f40f3e3c147730c2f0d410595b6aa777a2322
Quantumlabss <alex.spadyevans@icloud.com>
2024-03-05 18:24:37 -0700

    hj

--- Source/GameDevScifi30/FPSGameModeBase.cpp
+++ Source/GameDevScifi30/FPSGameModeBase.cpp
@@ -8,30 +8,39 @@
 #include "Kismet/GameplayStatics.h"
 #include <Subsystems/PanelExtensionSubsystem.h>
 
+
+
+
+
+	//:Super()
+
+
 void AFPSGameModeBase::StartPlay()
 {
 	Super::StartPlay();
 
 	check(GEngine != nullptr);
+	//#
+	//static ConstructorHelpers::FClassFinder<UUserWidget> HealthBar(TEXT("C:/Users/alex.evans/source/repos/GameDevScifi30/Content/Scripts/HUD"));
+//	HUDWidgetClass = HealthBar.Class;
 
+	// use our custom HUD class
+//	HUDClass = UFPHUD::StaticClass();
+
+	// add Health Bar UI to viewport
+//	if (HUDWidgetClass != nullptr)
+//	{
+//		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), HUDWidgetClass);
+
+//		if (CurrentWidget)
+//		{
+//			CurrentWidget->AddToViewport();
+//		}
+//	}
+#//
 	// Display a debug message for five seconds. 
 	// The -1 "Key" value argument prevents the message from being updated or refreshed.
 	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameMode!"));
 
-	static ConstructorHelpers::FClassFinder<UUserWidget> HealthBar(TEXT("C:/Users/alex.evans/source/repos/GameDevScifi30/Content/Scripts/HUD"));
-	HUDWidgetClass = HealthBar.Class;
 
-	// use our custom HUD class
-	HUDClass = UFPHUD::StaticClass();
-
-	// add Health Bar UI to viewport
-	if (HUDWidgetClass != nullptr)
-	{
-		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), HUDWidgetClass);
-
-		if (CurrentWidget)
-		{
-			CurrentWidget->AddToViewport();
-		}
-	}
 }
--- Source/GameDevScifi30/FPSGameModeBase.h
+++ Source/GameDevScifi30/FPSGameModeBase.h
@@ -14,18 +14,9 @@ class GAMEDEVSCIFI30_API AFPSGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 
+
+
 	virtual void StartPlay() override;
 
-public:
-	UFUNCTION(BlueprintPure, Category = "Health")
-//GamePlayState GetCurrentState() const;
 
-	/** Sets a new playing state */
-//oid SetCurrentState(EGamePlayState NewState);
-
-	UPROPERTY(EditAnywhere, Category = "Health")
-	TSubclassOf<class UUserWidget> HUDWidgetClass;
-
-	UPROPERTY(EditAnywhere, Category = "Health")
-	class UUserWidget* CurrentWidget;
 };
